263ecb546a420173c10956581621b100da76185d
unknown <patrickyuanto.123@gmail.com>
2024-06-26 08:08:54 +0800

    Changed Weather Widget to display current data; Added responsive design to weather cards

--- src/WeatherCard.jsx
+++ src/WeatherCard.jsx
@@ -3,8 +3,9 @@ export default function WeatherCard(props){
     return(<div className="weatherCard">
         
             <p>{props.date}</p>
-            <img src={props.forecastCondition.icon} alt={""}/>
-            <p>{props.forecastCondition.text}</p>
-
+            <img src={props.forecastDay.condition.icon} alt={""}/>
+            <p>{props.forecastDay.condition.text}</p>
+            <p>{props.forecastDay.avgtemp_c}&deg;C</p>
+            
     </div>)
 }
--- src/Widget.jsx
+++ src/Widget.jsx
@@ -1,21 +1,25 @@
 import WeatherCard from "./WeatherCard";
 
 export default function Widget(props){
+
+    console.log(props.data);
     
     var forecasts = props.data.forecast.forecastday;
     var location = props.data.location;
+    var current = props.data.current;
 
 
     return(
     <div className="weatherWidget"> 
     <div className="main">
         <h1>Weather Forecasts in {location.country}</h1>
-        <h3>{forecasts[0].date}</h3>
-        <img className ="icon" src={forecasts[0].day.condition.icon} alt={""}/>
-        <h2>{forecasts[0].day.condition.text}</h2>
+        <h3>{current.last_updated.split(' ')[0]}</h3>
+        <img className ="icon" src={current.condition.icon} alt={""}/>
+        <h2>{current.condition.text}</h2>
+        <h2>{current.temp_c}&deg;C</h2>
     </div>
         <ul className="weatherList">
-            {forecasts.map((forecast,index)=> index !== 0 ? <WeatherCard date={forecast.date} forecastCondition={forecast.day.condition} /> : null) }  
+            {forecasts.map((forecast,index)=> index !== 0 ? <WeatherCard date={forecast.date} forecastDay={forecast.day} /> : null) }  
         </ul>
     </div>)
 }
